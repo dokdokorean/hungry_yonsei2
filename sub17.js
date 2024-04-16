bcd5c69d1a6ed67633ed82b0e94e82b4eafb46db
dokdokorean <gravity0225@naver.com>
2024-04-16 11:58:10 +0900

    카페 지 운영시간 수정

--- sub17.js
+++ sub17.js
@@ -14,7 +14,7 @@ let min = date.getMinutes()/60;
 let hours=date.getHours()+min;
 
 if( day1 >0 && day1<6){
-    if (hours>=8.5 && hours < 19.5){
+    if (hours>=8&& hours < 18){
         document.getElementById('close').style.visibility="hidden"
         document.getElementById('open').style.visibility='visible'
 
@@ -60,29 +60,6 @@ if( day1 >0 && day1<6){
     }
 }
 else if(day1==6){
-    if (hours>=10.5 && hours < 14.5){
-        document.getElementById('close').style.visibility="hidden"
-        document.getElementById('open').style.visibility='visible'
-
-        document.getElementById('main2-2-1-1').style.fontFamily="Pretendard-medium"
-        document.getElementById('main2-2-1-2').style.fontFamily="Pretendard-light"
-
-        document.getElementById('main2-2-1-1').style.color="#4D4D4D"
-        document.getElementById('main2-2-1-2').style.color="#4D4D4D"
-
-        document.getElementById('main2-2-2-1').style.fontFamily="Pretendard-sb"
-        document.getElementById('main2-2-2-2').style.fontFamily="Pretendard-sb"
-
-        document.getElementById('main2-2-2-1').style.color="#34C759"
-        document.getElementById('main2-2-2-2').style.color="#34C759"
-
-        document.getElementById('main2-2-3-1').style.fontFamily="Pretendard-medium"
-        document.getElementById('main2-2-3-2').style.fontFamily="Pretendard-light"
-
-        document.getElementById('main2-2-3-1').style.color="#4D4D4D"
-        document.getElementById('main2-2-3-2').style.color="#4D4D4D"
-    }
-    else{
         document.getElementById('close').style.visibility="visible"
         document.getElementById('open').style.visibility='hidden'
 
@@ -103,7 +80,6 @@ else if(day1==6){
 
         document.getElementById('main2-2-3-1').style.color="#4D4D4D"
         document.getElementById('main2-2-3-2').style.color="#4D4D4D"
-    }
 }
 else if(day1==0){
         document.getElementById('close').style.visibility="visible"
