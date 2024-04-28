36137f64d4686fac5b3502ee409ed784c555e6eb
dokdokorean <gravity0225@naver.com>
2024-04-28 13:08:39 +0900

    메뉴 업데이트

--- weekly.js
+++ weekly.js
@@ -2,31 +2,31 @@ function menu_load(){
 //월요일
 
 //천원의 아침
-document.getElementById('mor-mon-menu1').innerText='야채하이라이스덮밥'
-document.getElementById('mor-mon-menu2').innerText='왕피자춘권*갈릭딥핑소스'
-document.getElementById('mor-mon-menu3').innerText='미니동그랑땡'
+document.getElementById('mor-mon-menu1').innerText='후르츠탕수육'
+document.getElementById('mor-mon-menu2').innerText='어묵숙주굴소스볶음'
+document.getElementById('mor-mon-menu3').innerText='바이오플레사과(1개)'
 
 document.getElementById('k-menu2-mon').innerText='-'//메뉴
 document.getElementById('k-menu3-mon').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-mon').innerText='사골우거지해장국'//메뉴
+document.getElementById('a-menu2-mon').innerText='돼지김치찌개'//메뉴
 document.getElementById('a-menu3-mon').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-mon').innerText='돼지순두부찌개'//메뉴
+document.getElementById('b-menu2-mon').innerText='잡채맛볶음밥*후라이'//메뉴
 document.getElementById('b-menu3-mon').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-mon').innerText='김.돈'//메뉴
+document.getElementById('c-menu2-mon').innerText='깻잎제육덮밥'//메뉴
 document.getElementById('c-menu3-mon').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-mon').innerText='가라아게동'//메뉴
+document.getElementById('d-menu2-mon').innerText='새우튀김우동'//메뉴
 document.getElementById('d-menu3-mon').innerText=''//가격
 
 //점심메뉴 6번 
-document.getElementById('e-menu2-mon').innerText='콩국수'//메뉴
+document.getElementById('e-menu2-mon').innerText='닭가슴살카레라이스'//메뉴
 document.getElementById('e-menu3-mon').innerText=''//가격
 
 //점심메뉴 7번
@@ -36,9 +36,9 @@ document.getElementById('f-menu3-mon').innerText=''//가격
 //화요일
 
 //천원의 아침
-document.getElementById('mor-tue-menu1').innerText='돼지고기숙주볶음'
-document.getElementById('mor-tue-menu2').innerText='꽃맛살메추리알샐러드'
-document.getElementById('mor-tue-menu3').innerText='식빵, 잼, 후라이'
+document.getElementById('mor-tue-menu1').innerText='떡갈비무침'
+document.getElementById('mor-tue-menu2').innerText='케이준샐러드'
+document.getElementById('mor-tue-menu3').innerText='베이비슈(1개한정)'
 
 
 //중식특식
@@ -50,19 +50,19 @@ document.getElementById('a-menu2-tue').innerText='소고기미역국'//메뉴
 document.getElementById('a-menu3-tue').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-tue').innerText='우삼겹된장찌개'//메뉴
+document.getElementById('b-menu2-tue').innerText='고추장찌개'//메뉴
 document.getElementById('b-menu3-tue').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-tue').innerText='닭갈비덮밥'//메뉴
+document.getElementById('c-menu2-tue').innerText='우삼겹스키야끼'//메뉴
 document.getElementById('c-menu3-tue').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-tue').innerText='특식 유산슬덮밥'//메뉴
+document.getElementById('d-menu2-tue').innerText='짜장면'//메뉴
 document.getElementById('d-menu3-tue').innerText=''//가격
 
 //점심메뉴 6번 
-document.getElementById('e-menu2-tue').innerText='베이컨갈릭볶음밥*어묵소떡'//메뉴
+document.getElementById('e-menu2-tue').innerText='중식 볶음밥'//메뉴
 document.getElementById('e-menu3-tue').innerText=''//가격
 
 //점심메뉴 7번 
@@ -72,9 +72,9 @@ document.getElementById('f-menu3-tue').innerText=''//가격
 //수요일
 
 //천원의 아침
-document.getElementById('mor-wed-menu1').innerText='된장찌개'
-document.getElementById('mor-wed-menu2').innerText='마늘간장떡갈비조림'
-document.getElementById('mor-wed-menu3').innerText='설탕건빵튀김'
+document.getElementById('mor-wed-menu1').innerText='돼지고기메추리알조림'
+document.getElementById('mor-wed-menu2').innerText='열대과일샐러드'
+document.getElementById('mor-wed-menu3').innerText='유부미소국'
 
 
 //중식
@@ -82,23 +82,23 @@ document.getElementById('k-menu2-wed').innerText='-'//메뉴
 document.getElementById('k-menu3-wed').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-wed').innerText='돼지김치찌개'//메뉴
+document.getElementById('a-menu2-wed').innerText='콩나물황태해장국'//메뉴
 document.getElementById('a-menu3-wed').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-wed').innerText='나물비빔밥'//메뉴
+document.getElementById('b-menu2-wed').innerText='삼겹짜글이'//메뉴
 document.getElementById('b-menu3-wed').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-wed').innerText='닭곰탕'//메뉴
+document.getElementById('c-menu2-wed').innerText='마파스틱덮밥'//메뉴
 document.getElementById('c-menu3-wed').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-wed').innerText='특식 스테이크동'//메뉴
+document.getElementById('d-menu2-wed').innerText='로제스파게티*새우볼꼬치'//메뉴
 document.getElementById('d-menu3-wed').innerText=''//가격
 
 //점심메뉴 6번 
-document.getElementById('e-menu2-wed').innerText='히야시라멘'//메뉴
+document.getElementById('e-menu2-wed').innerText='치킨데리야끼덮밥'//메뉴
 document.getElementById('e-menu3-wed').innerText=''//가격
 
 //점심메뉴 7번 
@@ -108,9 +108,9 @@ document.getElementById('f-menu3-wed').innerText=''//가격
 //목요일
 
 //천원의 아침
-document.getElementById('mor-thur-menu1').innerText='미역된장국'
-document.getElementById('mor-thur-menu2').innerText='스팸돈부리'
-document.getElementById('mor-thur-menu3').innerText='순두부*양념장'
+document.getElementById('mor-thur-menu1').innerText='모듬햄볶음'
+document.getElementById('mor-thur-menu2').innerText='쥐어채간장조림'
+document.getElementById('mor-thur-menu3').innerText='젤리뽀'
 
 
 //중식메뉴
@@ -118,23 +118,23 @@ document.getElementById('k-menu2-thur').innerText='-'//메뉴
 document.getElementById('k-menu3-thur').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-thur').innerText='얼큰소콩나물해장국'//메뉴
+document.getElementById('a-menu2-thur').innerText='꽃게탕'//메뉴
 document.getElementById('a-menu3-thur').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-thur').innerText='냉면'//메뉴
+document.getElementById('b-menu2-thur').innerText='물냉면'//메뉴
 document.getElementById('b-menu3-thur').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-thur').innerText='우삼겹규동'//메뉴
+document.getElementById('c-menu2-thur').innerText='돈까스김치나베'//메뉴
 document.getElementById('c-menu3-thur').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-thur').innerText='스팸마요덮밥'//메뉴
+document.getElementById('d-menu2-thur').innerText='부타동'//메뉴
 document.getElementById('d-menu3-thur').innerText=''//가격
 
 //점심메뉴 6번
-document.getElementById('e-menu2-thur').innerText='들깨수제비'//메뉴
+document.getElementById('e-menu2-thur').innerText='스팸돈부리'//메뉴
 document.getElementById('e-menu3-thur').innerText=''//가격
 
 //점심메뉴 7번 
@@ -143,9 +143,9 @@ document.getElementById('f-menu3-thur').innerText=''//가격
 
 //금요일
 //천원의 아침
-document.getElementById('mor-fri-menu1').innerText='비지찌개'
-document.getElementById('mor-fri-menu2').innerText='햄감자채볶음'
-document.getElementById('mor-fri-menu3').innerText='알새우칩'
+document.getElementById('mor-fri-menu1').innerText='고구마맛탕'
+document.getElementById('mor-fri-menu2').innerText='요거트(80g)*시리얼'
+document.getElementById('mor-fri-menu3').innerText='도시락김, 후라이'
 
 
 //중식메뉴
@@ -153,19 +153,19 @@ document.getElementById('k-menu2-fri').innerText='-'//메뉴
 document.getElementById('k-menu3-fri').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-fri').innerText='순대국밥'//메뉴
+document.getElementById('a-menu2-fri').innerText='만두칼국수'//메뉴
 document.getElementById('a-menu3-fri').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-fri').innerText='중화 잡채밥'//메뉴
+document.getElementById('b-menu2-fri').innerText='변동 가능'//메뉴
 document.getElementById('b-menu3-fri').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-fri').innerText='모듬까스'//메뉴
+document.getElementById('c-menu2-fri').innerText='변동 가능'//메뉴
 document.getElementById('c-menu3-fri').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-fri').innerText=''//메뉴
+document.getElementById('d-menu2-fri').innerText='변동 가능'//메뉴
 document.getElementById('d-menu3-fri').innerText=''//가격
 
 //점심메뉴 6번
