8c23ad219adac75148b84dbb83b98efd314c8e7c
dokdokorean <gravity0225@naver.com>
2024-05-19 21:28:53 +0900

    메뉴 수정

--- weekly.js
+++ weekly.js
@@ -19,7 +19,7 @@ document.getElementById('b-menu2-mon').innerText='지코바덮밥'//메뉴
 document.getElementById('b-menu3-mon').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-mon').innerText='베이컨감자볶음밥*사워크림S'//메뉴
+document.getElementById('c-menu2-mon').innerText='베이컨감자볶음밥'//메뉴
 document.getElementById('c-menu3-mon').innerText=''//가격
 
 //점심메뉴 5번 
@@ -27,7 +27,7 @@ document.getElementById('d-menu2-mon').innerText='돼지김치찜덮밥'//메뉴
 document.getElementById('d-menu3-mon').innerText=''//가격
 
 //점심메뉴 6번 
-document.getElementById('e-menu2-mon').innerText='소고기미역국 아영'//메뉴
+document.getElementById('e-menu2-mon').innerText='소고기미역국'//메뉴
 document.getElementById('e-menu3-mon').innerText=''//가격
 
 //점심메뉴 7번
@@ -51,7 +51,7 @@ document.getElementById('a-menu2-tue').innerText='가라아게동'//메뉴
 document.getElementById('a-menu3-tue').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-tue').innerText='사골우거지해장국 아영'//메뉴
+document.getElementById('b-menu2-tue').innerText='사골우거지해장국'//메뉴
 document.getElementById('b-menu3-tue').innerText=''//가격
 
 //점심메뉴 4번
@@ -83,7 +83,7 @@ document.getElementById('k-menu2-wed').innerText='-'//메뉴
 document.getElementById('k-menu3-wed').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-wed').innerText='돼지김치찌개 아영'//메뉴
+document.getElementById('a-menu2-wed').innerText='돼지김치찌개'//메뉴
 document.getElementById('a-menu3-wed').innerText=''//가격
 
 //점심메뉴 3번 
@@ -91,7 +91,7 @@ document.getElementById('b-menu2-wed').innerText='간장찜닭덮밥'//메뉴
 document.getElementById('b-menu3-wed').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-wed').innerText='소불고기볶음밥*계란후라이'//메뉴
+document.getElementById('c-menu2-wed').innerText='소불고기볶음밥'//메뉴
 document.getElementById('c-menu3-wed').innerText=''//가격
 
 //점심메뉴 5번 
@@ -111,7 +111,7 @@ document.getElementById('f-menu3-wed').innerText=''//가격
 //천원의 아침
 document.getElementById('mor-thur-menu1').innerText='전주식콩나물해장국'
 document.getElementById('mor-thur-menu2').innerText='탕평채'
-document.getElementById('mor-thur-menu3').innerText='미니찐빵튀김(2개)*연유'
+document.getElementById('mor-thur-menu3').innerText='미니찐빵튀김(2개)'
 
 
 //중식메뉴
@@ -119,11 +119,11 @@ document.getElementById('k-menu2-thur').innerText='-'//메뉴
 document.getElementById('k-menu3-thur').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-thur').innerText='특 삼계탕 (50그릇 한정)'//메뉴
+document.getElementById('a-menu2-thur').innerText='삼계탕(50그릇 한정)'//메뉴
 document.getElementById('a-menu3-thur').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-thur').innerText='특 흑임자삼계탕(50그릇 한정)'//메뉴
+document.getElementById('b-menu2-thur').innerText='흑임자삼계탕(50그릇 한정)'//메뉴
 document.getElementById('b-menu3-thur').innerText=''//가격
 
 //점심메뉴 4번
@@ -131,7 +131,7 @@ document.getElementById('c-menu2-thur').innerText='김치볶음밥*빅핫도그'
 document.getElementById('c-menu3-thur').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-thur').innerText='경상도식소고기무국 아영'//메뉴
+document.getElementById('d-menu2-thur').innerText='경상도식소고기무국'//메뉴
 document.getElementById('d-menu3-thur').innerText=''//가격
 
 //점심메뉴 6번
@@ -158,7 +158,7 @@ document.getElementById('a-menu2-fri').innerText='우렁쌈밥정식'//메뉴
 document.getElementById('a-menu3-fri').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-fri').innerText='계란볶음밥(칠리S)*돈까스'//메뉴
+document.getElementById('b-menu2-fri').innerText='계란볶음밥*돈까스'//메뉴
 document.getElementById('b-menu3-fri').innerText=''//가격
 
 //점심메뉴 4번
