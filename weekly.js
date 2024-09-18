f433ddbb4f66e8693ff90e9daa143ae0f93eede8
dokdokorean <gravity0225@naver.com>
2024-09-19 01:22:10 +0900

    메뉴 업데이트

--- weekly.js
+++ weekly.js
@@ -118,12 +118,12 @@ document.getElementById('f-menu3-wed').innerText=''//가격
 //목요일
 
 //천원의 아침
-document.getElementById('mor-thur-menu1').innerText=''
-document.getElementById('mor-thur-menu2').innerText=''
-document.getElementById('mor-thur-menu3').innerText=''
-document.getElementById('mor-thur-menu4').innerText=''
-document.getElementById('mor-thur-menu5').innerText=''
-document.getElementById('mor-thur-menu6').innerText=''
+document.getElementById('mor-thur-menu1').innerText='콩나물김치국'
+document.getElementById('mor-thur-menu2').innerText='백미밥'
+document.getElementById('mor-thur-menu3').innerText='순대볶음'
+document.getElementById('mor-thur-menu4').innerText='생선까스&타르타르소스'
+document.getElementById('mor-thur-menu5').innerText='배추김치/요구르트'
+document.getElementById('mor-thur-menu6').innerText='셀프계란후라이'
 
 
 //반찬메뉴
@@ -131,19 +131,19 @@ document.getElementById('k-menu2-thur').innerText=''//메뉴
 document.getElementById('k-menu3-thur').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-thur').innerText='수요일 이후 제공 예정'//메뉴
+document.getElementById('a-menu2-thur').innerText='소고기미역국'//메뉴
 document.getElementById('a-menu3-thur').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-thur').innerText=''//메뉴
+document.getElementById('b-menu2-thur').innerText='돈까스김치나베'//메뉴
 document.getElementById('b-menu3-thur').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-thur').innerText=''//메뉴
+document.getElementById('c-menu2-thur').innerText='새우계란볶음밥'//메뉴
 document.getElementById('c-menu3-thur').innerText=''//가격
 
 //점심메뉴 5번 
-document.getElementById('d-menu2-thur').innerText=''//메뉴
+document.getElementById('d-menu2-thur').innerText='우삽겹숙주덮밥'//메뉴
 document.getElementById('d-menu3-thur').innerText=''//가격
 
 //점심메뉴 6번
@@ -151,17 +151,17 @@ document.getElementById('e-menu2-thur').innerText=''//메뉴
 document.getElementById('e-menu3-thur').innerText=''//가격
 
 //점심메뉴 7번 
-document.getElementById('f-menu2-thur').innerText=''//메뉴
+document.getElementById('f-menu2-thur').innerText='우동장국'//메뉴
 document.getElementById('f-menu3-thur').innerText=''//가격
 
 //금요일
 //천원의 아침
-document.getElementById('mor-fri-menu1').innerText=''
-document.getElementById('mor-fri-menu2').innerText=''
-document.getElementById('mor-fri-menu3').innerText=''
-document.getElementById('mor-fri-menu4').innerText=''
-document.getElementById('mor-fri-menu5').innerText=''
-document.getElementById('mor-fri-menu6').innerText=''
+document.getElementById('mor-fri-menu1').innerText='근대된장국'
+document.getElementById('mor-fri-menu2').innerText='백미밥'
+document.getElementById('mor-fri-menu3').innerText='돼지간장불고기'
+document.getElementById('mor-fri-menu4').innerText='연두부&조미간장'
+document.getElementById('mor-fri-menu5').innerText='배추김치/도시락김'
+document.getElementById('mor-fri-menu6').innerText='셀프계란후라이'
 
 
 //반찬메뉴
@@ -169,15 +169,15 @@ document.getElementById('k-menu2-fri').innerText=''//메뉴
 document.getElementById('k-menu3-fri').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-fri').innerText='수요일 이후 제공 예정'//메뉴
+document.getElementById('a-menu2-fri').innerText='육개장'//메뉴
 document.getElementById('a-menu3-fri').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-fri').innerText=''//메뉴
+document.getElementById('b-menu2-fri').innerText='크리미양파돈까스'//메뉴
 document.getElementById('b-menu3-fri').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-fri').innerText=''//메뉴
+document.getElementById('c-menu2-fri').innerText='메뉴 미정'//메뉴
 document.getElementById('c-menu3-fri').innerText=''//가격
 
 //점심메뉴 5번 
@@ -189,6 +189,6 @@ document.getElementById('e-menu2-fri').innerText=''//메뉴
 document.getElementById('e-menu3-fri').innerText=''//가격
 
 //점심메뉴 7번 
-document.getElementById('f-menu2-fri').innerText=''//메뉴
+document.getElementById('f-menu2-fri').innerText='우동장국'//메뉴
 document.getElementById('f-menu3-fri').innerText=''//가격
 }
