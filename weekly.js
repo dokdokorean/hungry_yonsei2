ae207dcd7763c2d53e5f0bb19021551d16f5d423
배진우 <baejinwoo@MacBook-Pro-2.local>
2025-02-23 22:11:16 +0900

    마지막 메뉴 업데이트

--- weekly.js
+++ weekly.js
@@ -10,19 +10,19 @@ document.getElementById('mor-mon-menu5').innerText=''
 document.getElementById('mor-mon-menu6').innerText=''
 
 //반찬특식
-document.getElementById('k-menu2-mon').innerText='어묵강정,콩나물무침,배추김치'//메뉴
+document.getElementById('k-menu2-mon').innerText='OT로 인해 일반식'//메뉴
 document.getElementById('k-menu3-mon').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-mon').innerText='I얼큰순대국밥'//메뉴
+document.getElementById('a-menu2-mon').innerText='미운영'//메뉴
 document.getElementById('a-menu3-mon').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-mon').innerText='C소고기미역국'//메뉴
+document.getElementById('b-menu2-mon').innerText=''//메뉴
 document.getElementById('b-menu3-mon').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-mon').innerText='E햄야채볶음밥&샤워크림'//메뉴
+document.getElementById('c-menu2-mon').innerText=''//메뉴
 document.getElementById('c-menu3-mon').innerText=''//가격
 
 //점심메뉴 5번 
@@ -49,19 +49,19 @@ document.getElementById('mor-tue-menu6').innerText=''
 
 
 //반찬특식
-document.getElementById('k-menu2-tue').innerText='계란옷소세지,콩자반,배추김치'//메뉴
+document.getElementById('k-menu2-tue').innerText='OT로 인해 일반식'//메뉴
 document.getElementById('k-menu3-tue').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-tue').innerText='D비지찌개'//메뉴
+document.getElementById('a-menu2-tue').innerText='미운영'//메뉴
 document.getElementById('a-menu3-tue').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-tue').innerText='F제육덮밥'//메뉴
+document.getElementById('b-menu2-tue').innerText=''//메뉴
 document.getElementById('b-menu3-tue').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-tue').innerText='E스팸마요덮밥'//메뉴
+document.getElementById('c-menu2-tue').innerText=''//메뉴
 document.getElementById('c-menu3-tue').innerText=''//가격
 
 //점심메뉴 5번 
@@ -88,19 +88,19 @@ document.getElementById('mor-wed-menu6').innerText=''
 
 
 //반찬
-document.getElementById('k-menu2-wed').innerText='교자만두,도시락김, 배추김치'//메뉴
+document.getElementById('k-menu2-wed').innerText='OT로 인해 일반식'//메뉴
 document.getElementById('k-menu3-wed').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-wed').innerText='I육개장'//메뉴
+document.getElementById('a-menu2-wed').innerText='미운영'//메뉴
 document.getElementById('a-menu3-wed').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-wed').innerText='F닭곰탕'//메뉴
+document.getElementById('b-menu2-wed').innerText=''//메뉴
 document.getElementById('b-menu3-wed').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-wed').innerText='B새우볶음밥'//메뉴
+document.getElementById('c-menu2-wed').innerText=''//메뉴
 document.getElementById('c-menu3-wed').innerText=''//가격
 
 //점심메뉴 5번 
@@ -127,19 +127,19 @@ document.getElementById('mor-thur-menu6').innerText=''
 
 
 //반찬메뉴
-document.getElementById('k-menu2-thur').innerText='비엔나야채볶음,무말랭이, 배추김치'//메뉴
+document.getElementById('k-menu2-thur').innerText='OT로 인해 일반식'//메뉴
 document.getElementById('k-menu3-thur').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-thur').innerText='E우삼겹된장찌개'//메뉴
+document.getElementById('a-menu2-thur').innerText='미운영'//메뉴
 document.getElementById('a-menu3-thur').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-thur').innerText='G마파두부덮밥'//메뉴
+document.getElementById('b-menu2-thur').innerText=''//메뉴
 document.getElementById('b-menu3-thur').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-thur').innerText='D잡채볶음밥'//메뉴
+document.getElementById('c-menu2-thur').innerText=''//메뉴
 document.getElementById('c-menu3-thur').innerText=''//가격
 
 //점심메뉴 5번 
@@ -165,19 +165,19 @@ document.getElementById('mor-fri-menu6').innerText=''
 
 
 //반찬메뉴
-document.getElementById('k-menu2-fri').innerText='미니돈까스,단무지,배추김치'//메뉴
+document.getElementById('k-menu2-fri').innerText='OT로 인해 일반식'//메뉴
 document.getElementById('k-menu3-fri').innerText=''//가격
 
 //점심메뉴 2번 
-document.getElementById('a-menu2-fri').innerText='E돼지고기순두부찌개'//메뉴
+document.getElementById('a-menu2-fri').innerText='미운영'//메뉴
 document.getElementById('a-menu3-fri').innerText=''//가격
 
 //점심메뉴 3번 
-document.getElementById('b-menu2-fri').innerText='D사골우거지해장국'//메뉴
+document.getElementById('b-menu2-fri').innerText=''//메뉴
 document.getElementById('b-menu3-fri').innerText=''//가격
 
 //점심메뉴 4번
-document.getElementById('c-menu2-fri').innerText='B짜장밥'//메뉴
+document.getElementById('c-menu2-fri').innerText=''//메뉴
 document.getElementById('c-menu3-fri').innerText=''//가격
 
 //점심메뉴 5번 
